552bb2b903b52b6245ee03dfbed9d42f0fd05826
jay98patel <itsj10.jey@gmail.com>
2021-04-26 09:41:30 +0530

    routing of quiz

--- src/app/modules/quiz/quiz-routing.module.ts
+++ src/app/modules/quiz/quiz-routing.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdvertisementComponent } from './components/advertisement/advertisement.component';
 import { QuizIntroductionComponent } from './components/quiz-introduction/quiz-introduction.component';
+import { QuizTipsComponent } from './components/quiz-tips/quiz-tips.component';
 import { QuizComponent } from './quiz.component';
 
 
 const routes: Routes = [
   {
-    path:'',component:QuizIntroductionComponent
+    path: '', component: QuizComponent,
   },
   {
-    path:'quiz',component:QuizIntroductionComponent
+    path: 'quizIntroduction', component: QuizIntroductionComponent
   },
   {
-
+    path: 'quizTip', component: QuizTipsComponent
+  },
+  {
+    path: 'advertisement', component: AdvertisementComponent
+  },
+  {
+    path: 'playQuiz', component: AdvertisementComponent
   }
 ];
 
--- src/app/modules/quiz/quiz.module.ts
+++ src/app/modules/quiz/quiz.module.ts
@@ -3,15 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { QuizRoutingModule } from './quiz-routing.module';
 import { QuizComponent } from './quiz.component';
-import { QuizIntroductionComponent } from './components/quiz-introduction/quiz-introduction.component';
 import { AdvertisementComponent } from './components/advertisement/advertisement.component';
 import { QuizTipsComponent } from './components/quiz-tips/quiz-tips.component';
 import { QuizfooterComponent } from './components/quizfooter/quizfooter.component';
 import { PlayQuizComponent } from './components/play-quiz/play-quiz.component';
+import { QuizIntroductionComponent } from './components/quiz-introduction/quiz-introduction.component';
 
 
 @NgModule({
-  declarations: [QuizComponent, QuizIntroductionComponent, AdvertisementComponent, QuizTipsComponent, QuizfooterComponent, PlayQuizComponent],
+  declarations: [QuizComponent, AdvertisementComponent, QuizTipsComponent, QuizfooterComponent, PlayQuizComponent,QuizIntroductionComponent],
   imports: [
     CommonModule,
     QuizRoutingModule
