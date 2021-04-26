f35d12e3f48a6129cf6c1aae8fcfc1b3472da9a5
jay98patel <itsj10.jey@gmail.com>
2021-04-26 10:13:49 +0530

    redirected default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-    {
-      path:'quiz',loadChildren:() => import ('./modules/quiz/quiz.module').then(m=>m.QuizModule)
-    },
-    {
-      path:'progress',loadChildren:() => import('./modules/progress/progress.module').then(m=>m.ProgressModule)
-    },
-    {
-      path:'writing',loadChildren:() => import('./modules/writings/writings.module').then(m=>m.WritingsModule)
-    },
-    {
-      path:'instructors',loadChildren:() => import('./modules/instructors/instructors.module').then(m=>m.InstructorsModule)
-    }
+  {
+    path: '', redirectTo: 'quiz', pathMatch: 'full'
+  },
+  {
+    path: 'quiz', loadChildren: () => import('./modules/quiz/quiz.module').then(m => m.QuizModule)
+  },
+  {
+    path: 'progress', loadChildren: () => import('./modules/progress/progress.module').then(m => m.ProgressModule)
+  },
+  {
+    path: 'writing', loadChildren: () => import('./modules/writings/writings.module').then(m => m.WritingsModule)
+  },
+  {
+    path: 'instructors', loadChildren: () => import('./modules/instructors/instructors.module').then(m => m.InstructorsModule)
+  },
+  
 ];
 
 @NgModule({
